161d392ef20acf4e524f53e21b505d149d4a7bcc
Vicente Del Fierro <vicente.delfierro@uc.cl>
2022-10-27 17:26:12 -0300

    suzuki

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # peru
 from kivy.app import App    
-from kivy.uix.label import Label 
+from kivy.uix.label import Label
+from kivy.utils import platform
 
 
 
 class Main(App):
     def build(self):
-        return Label(text="ASTRAVAM 7w7")
+        return Label(text=f"ASTRAVAM 7w7\nEstas en {platform}")
 
 Main().run()
