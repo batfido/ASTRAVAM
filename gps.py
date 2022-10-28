532e983a87719e4b0fd57d51d43e6838b3cfac30
ercaceres <69520684+ercaceres@users.noreply.github.com>
2022-10-27 21:30:28 -0300

    uwu
    owo

--- gps.py
+++ gps.py
@@ -1,15 +1,51 @@
-from cgitb import text
-from kivy.app import App    
-from kivy.uix.label import Label
-from kivy.utils import platform
 from plyer import gps
-from kivymd.uix.dialog import MDDialog
+import time
 
-class GPSConf():
-    def run(*args,**kwargs):
-        # Permisos: ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION
-        print("boton apretado")
-        dialog = MDDialog(title="Equisde", text= "Aun no hay codigo para esto")
-        dialog.open()
-        pass
+class Gps:
+    def __init__(self):
+        try:
+            print("comenzando el Gps")
+            self.disco_pare = [-33.500195465448776, -70.61154519632926]
+            self.alerta = [-33.500163752818224, -70.61100120164757]
+            self.datos_relativos = ''
+            self.comienza_gps()
+            print("Gps listo")
+        except:
+            print('Fallo al activar el gps, revise su conexion o plataforma')
         
+    def datos_gps(self, **kwargs):
+        datos = f'{lat}, {lon}, {alt}, {spd}'.format(**kwargs)
+        cadena = str(datos)
+        cadenagps = cadena.split(',')
+        lat = cadenagps[0]
+        lon = cadenagps[1]
+        alt = cadenagps[2]
+        spd = cadenagps[3]
+
+        print (f"latitud: {lat} \n longitud: {lon} \n altitude: {alt} \n speed: {spd} \r\n")
+        self.datos_relativos = "\t"+lat+"\t"+lon+"\t"+alt+"\t"+spd+"\t\r\n"
+        self.bufergps()
+
+    def bufer_gps(self):
+        self.comparar_ubicacion(self.datos_relativos)
+        return self.datos_relativos
+    def comienza_gps(self, *args):
+        try:
+            gps.configure(on_location=self.datos_gps)
+            if True:
+                gps.start()
+                self.datos_relativos=self.bufer_gps()
+        except:
+            self.datos_relativos = 0
+            print(self.datos_relativos)
+            pass
+        return self.datos_relativos
+
+    def comparar_ubicacion(self, datos):
+        if datos[0] == str(self.alerta[0]) and datos[1] == str(self.alerta[1]):
+            print('hay un disco pare a 50 metros')
+        else:
+            print('no hay disco pare cercano')
+
+    def __del__(self):
+        print('hola soy el destructor de la clase gps')
--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-# peru
-import imp
-from operator import pos
-from turtle import bgcolor, color
 from kivy.app import App
 from kivymd.app import MDApp
-from kivy.uix.label import Label
-from kivy.utils import platform
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
 from plyer import gps
-from kivymd.uix.button import MDRectangleFlatButton
-from kivymd.uix.screen import MDScreen
-from kivymd.uix.label import MDLabel
-from gps import GPSConf
+from gps import Gps
 
+class Ui(ScreenManager):
+    pass
 
-
-class Main(MDApp):
+class MainApp(MDApp):
     def build(self):
-        screen = MDScreen()
-        astralabel = MDLabel(text=f"ASTRAVAM 7w7\nEstas en {platform}", pos={0.5, 0.2})
-        screen.add_widget(astralabel)
-        start_btn=MDRectangleFlatButton(text="Comienza!", pos_hint={"center_x": 0.5, "center_y": 0.5})
-        screen.add_widget(start_btn)
-        start_btn.bind(on_press = GPSConf.run)
-        return screen
-Main().run()
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.primary_palette = 'Teal'
+        Builder.load_file('design.kv')
+        return Ui()
+    pass
+    def change_switch(self, checked, value):
+        if value:
+            Gps()
+            self.theme_cls.theme_style = 'Light'
+            self.theme_cls.primary_palette = 'Red'
+        else:
+            self.theme_cls.theme_style = 'Dark'
+            self.theme_cls.primary_palette = 'Teal'
+            pass
+        
+if __name__ == '__main__':
+    MainApp().run()
+
