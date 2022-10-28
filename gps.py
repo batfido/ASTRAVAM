5124207f14b317129d6a2f6e363b45edecbe6230
Vicente Del Fierro <vicente.delfierro@uc.cl>
2022-10-27 23:50:32 -0300

    hyundai

--- gps.py
+++ gps.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.utils import platform
 from kivymd.uix.dialog import MDDialog
+from plyer import gps
 
 class GPSConf():
     def __init__(self):
@@ -26,9 +27,7 @@ class PermManager():
         if not App.get_running_app().gps_stat:
             app = App.get_running_app()
         #   if platform == "android":
-            if platform != "android":
-                app.gps_label.text ="El dispositivo no tiene soporte GPS"
-            else:
+            if platform == "android":
                 from android.permissions import Permission, request_permissions
                 def callback(permission, results):
                     # CALLBACK PARA PERMISOS, REVISA PERMISOS OTORGADOS
@@ -38,9 +37,22 @@ class PermManager():
                         print("Permisos no otorgados")
                 request_permissions([Permission.ACCESS_COARSE_LOCATION, Permission.ACCESS_FINE_LOCATION], 
                                     callback)
+            else:
+                app.gps_label.text ="El dispositivo no tiene soporte GPS"
+            
         else:
             pass
 
+class WinCheeser():
+    def __init__(self, *args, **kwargs):
+        return self
+
+    def set_location(*args, **kwargs):
+        app = App.get_running_app()
+        app.location["lat"]=float(input("Lat"))
+        app.lat_label.text=str(app.location["lat"])
+        app.location["lon"]=float(input("Lon"))
+        app.lon_label.text=str(app.location["lon"])
 
 
 
@@ -48,14 +60,25 @@ class GPSOperator():
     def __init__(self) -> None:
         pass
     def start_gps(self, *args, **kwargs):
-        from plyer import gps
         perm_manager=PermManager()
+        app = App.get_running_app()
+        win_cheeser = WinCheeser
         try:
             gps.configure(on_location=self.update_loc, on_status=perm_manager.resolve_permissions)
             gps.start(minTime=100)
         except NotImplementedError or ModuleNotFoundError:
             diag=MDDialog(title="GPS no implementado", text="No se detecta un GPS utilizable por el sistema")
             diag.open()
+            app.gps_label.pos_hint={"center_x": 1.08, "center_y": 0.15}
+            app.gps_label.text= "No se detecta un GPS utilizable por el sistema"
+            win_cheeser.set_location()
+            app.cheese_btn=MDRectangleFlatButton(text="Revisa alertas", pos_hint={"center_x": 0.5, "center_y": 0.3})
+            app.add_widget(app.cheese_btn)
+            app.cheese_btn.bind(on_press = check_gps)
+    def check_gps():
+        print("eyacula")
+
+
 
     def update_loc(self, *args, **kwargs):
         app = App.get_running_app()
@@ -64,4 +87,3 @@ class GPSOperator():
         app.gps_label.text =f"Ubicación: Lat:{my_lat} Lon:{my_lon}"
         app.location["lat"]=my_lat
         app.location["lon"]=my_lon
-        
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# peru
 from operator import pos
 from turtle import bgcolor, color
 from kivy.app import App
@@ -16,13 +15,17 @@ gpsd=GPSConf
 class Main(MDApp):
     def build(self):
         self.gps_stat=False
-        self.location=dict()
+        self.location={"lat": "NA", "lon": "NA"}
         self.screen = MDScreen()
-        self.gps_label=MDLabel(text="No hay informacion del GPS", pos_hint={"center_x": 1.2, "center_y": 0.05})
+        self.gps_label=MDLabel(text="No hay informacion del GPS", pos_hint={"center_x": 1.2, "center_y": 0.15})
         self.screen.add_widget(self.gps_label)
         self.start_btn=MDRectangleFlatButton(text="Comienza!", pos_hint={"center_x": 0.5, "center_y": 0.5})
         self.screen.add_widget(self.start_btn)
         self.start_btn.bind(on_press = self.call_gps)
+        self.lat_label=MDLabel(text=self.location["lat"], pos_hint={"center_x": 1.2, "center_y": 0.1})
+        self.screen.add_widget(self.lat_label)
+        self.lon_label=MDLabel(text=self.location["lon"], pos_hint={"center_x": 1.2, "center_y": 0.05})
+        self.screen.add_widget(self.lon_label)
         return self.screen
     def call_gps(self,*args):
         gpsd.run()
